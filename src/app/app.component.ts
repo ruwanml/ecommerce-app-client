b64fa13b1d569e5d3529519934331607d071b812
ruwanm <ruwan.madanayake@vitalhub.com>
2024-08-14 17:19:14 +0530

    3. Develop TypeScript class for Product 4. Create Angular service to call REST APIs Add app.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ProductListComponent } from "./components/product-list/product-list.component";
+//import { RouterOutlet } from '@angular/router';
+//import { ProductListComponent } from "./components/product-list/product-list.component";
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, ProductListComponent],
+  //standalone: true,
+  //imports: [RouterOutlet, ProductListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-product-list',
-  standalone: true,
-  imports: [],
+  //standalone: true,
+  //imports: [],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
